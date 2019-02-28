a55dcb3d8d6e1ca3c3e76f31fe099af9ceb4f8e6
masood <masood@VMT4532.VALUEMOMENTUM.BIZ>
2019-02-28 14:24:51 +0530

    JenkinTest

---
+++ src/main/java/testingJenkins/testingJenkins.java
@@ -0,0 +1,15 @@
+package testingJenkins;
+
+import org.testng.annotations.Test;
+
+public class testingJenkins {
+	
+	
+	@Test
+	
+	public void LoginTest() {
+		
+		System.out.println("LoginTest");
+	}
+
+}
